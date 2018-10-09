9c65950920437f1e14b412442949811936959a9e
Unknown <kimjh6980@naver.com>
2018-10-09 20:38:35 +0900

    BusBoardingUpdate

---
+++ Assets/Script/BodyColliderEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyColliderEvent : MonoBehaviour {
+
+	public GameObject[] ring = new GameObject[10];
+    private Animator BusDoorAni;
+    private Animator BusAni;
+
+    private void Awake()
+    {
+        BusDoorAni = GameObject.Find("BusAddInner").GetComponent<Animator>();
+        BusAni = GameObject.Find("Player&Bus").GetComponent<Animator>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other.CompareTag("TP_Ring"))
+        {
+            
+            switch(other.name)
+            {
+                case "R1":
+                    Debug.Log("TP_Ring R1");
+                    ring[0].SetActive(false);
+                    BusDoorAni.SetInteger("StatusNum", 2);
+                    BusAni.SetInteger("BusRun", 1);
+                    //ring[1].SetActive(true);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Script/VechielTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VechielTrigger : MonoBehaviour {
+
+    public GameObject[] CanvasList = new GameObject[10];
+    public GameObject[] EventCube = new GameObject[10];
+    public GameObject[] Particle = new GameObject[10];
+
+    private Animator BusAni;
+    private void Awake()
+    {
+        BusAni = GameObject.Find("BusAddInner").GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        BusAni.SetInteger("StatusNum", 1);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("EventTag"))
+        {
+            switch (other.name)
+            {
+                case "E0":
+                    CanvasList[0].SetActive(false);
+                    CanvasList[1].SetActive(true);
+                    EventCube[0].SetActive(false);
+                    Particle[0].SetActive(true);
+                    break;
+                case "E1":
+                    CanvasList[1].SetActive(false);
+                    CanvasList[2].SetActive(true);
+                    EventCube[1].SetActive(false);
+                    break;
+            }
+        }
+    }
+}
