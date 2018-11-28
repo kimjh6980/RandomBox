2d09a52e8183108711d023eee50c56cf2eaf42f7
Yunsik <dkapqk1124@gmail.com>
2018-11-28 13:49:52 +0900

    start animation add

---
+++ Assets/ani.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ani : MonoBehaviour {
+
+    public Animation tr;
+    public Animation per;
+    public Animation sw;
+
+	// Use this for initialization
+	void Start () {
+
+        tr.Play("movetrain");
+        per.Play("movechar");
+        sw.Play("movesw2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/startani.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startani : MonoBehaviour {
+
+    public Animation train;
+    public Animation person;
+    public Animation sw;
+
+	// Use this for initialization
+	void Start () {
+        train.Play();
+        person.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
