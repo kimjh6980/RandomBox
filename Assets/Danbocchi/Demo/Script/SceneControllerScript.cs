b59cc9a86514073689f13268469f3a6a7040bd63
Yunsik <dkapqk1124@gmail.com>
2018-11-28 18:51:03 +0900

    glassdoor

---
+++ Assets/Danbocchi/Demo/Script/SceneControllerScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneControllerScript : MonoBehaviour {
+	public GameObject DanbocchiObject;
+	public GameObject DoorObject;
+	public GameObject FrontPanelObject;
+	public GameObject RightPanelObject;
+	public GameObject LeftPanelObject;
+	public GameObject RearPanelObject;
+	public GameObject RoofPanelObject;
+	public GameObject SilencerObject;
+
+	bool rotateFlag = false;
+	bool doorFlag = false;
+	bool panelFlag = false;
+	float speed = 0.02f;
+	float dt, pt;
+	Vector3 frontPanelPos;
+	Vector3 rightPanelPos;
+	Vector3 leftPanelPos;
+	Vector3 rearPanelPos;
+	Vector3 roofPanelPos;
+	Vector3 silencerPos;
+
+	// Use this for initialization
+	void Start () {
+		dt = 1f;
+		pt = 1f;
+		frontPanelPos = FrontPanelObject.transform.localPosition;
+		rightPanelPos = RightPanelObject.transform.localPosition;
+		leftPanelPos = LeftPanelObject.transform.localPosition;
+		rearPanelPos = RearPanelObject.transform.localPosition;
+		roofPanelPos = RoofPanelObject.transform.localPosition;
+		silencerPos = SilencerObject.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dt += speed;
+		Mathf.Clamp01 (dt);
+		float doorAngle;
+		if (doorFlag) {
+			doorAngle = Mathf.LerpAngle (0f, 120f, dt);
+		} else {
+			doorAngle = Mathf.LerpAngle (120f, 0f, dt);
+		}
+		DoorObject.transform.localEulerAngles = new Vector3 (0, doorAngle, 0);
+
+		if (rotateFlag) {
+			DanbocchiObject.transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
+		}
+
+		pt += speed;
+		Mathf.Clamp01 (pt);
+		float panelDistance;
+		if (panelFlag) {
+			panelDistance = Mathf.Lerp (0f, 0.2f, pt);
+		} else {
+			panelDistance = Mathf.Lerp (0.2f, 0f, pt);
+		}
+		FrontPanelObject.transform.localPosition = new Vector3 (frontPanelPos.x, frontPanelPos.y, frontPanelPos.z - panelDistance);
+		RightPanelObject.transform.localPosition = new Vector3 (rightPanelPos.x + panelDistance, rightPanelPos.y, rightPanelPos.z);
+		LeftPanelObject.transform.localPosition = new Vector3 (leftPanelPos.x - panelDistance, leftPanelPos.y, leftPanelPos.z);
+		RearPanelObject.transform.localPosition = new Vector3 (rearPanelPos.x , rearPanelPos.y, rearPanelPos.z + panelDistance);
+		RoofPanelObject.transform.localPosition = new Vector3 (roofPanelPos.x , roofPanelPos.y + panelDistance, roofPanelPos.z);
+		SilencerObject.transform.localPosition = new Vector3 (silencerPos.x , silencerPos.y + (panelDistance * 2f), silencerPos.z);
+	}
+
+	public void OnDoorButton () {
+		dt = 0f;
+		doorFlag = !doorFlag;
+	}
+
+	public void OnRotateButton() {
+		rotateFlag = !rotateFlag;
+	}
+
+	public void OnAssembleButton() {
+		pt = 0f;
+		panelFlag = !panelFlag;
+	}
+}
---
+++ Assets/Danbocchi/readme_en.txt
@@ -0,0 +1,21 @@
+
+DANBOCCHI | The simple cardboard soundproof room.
+version 1.0
+
+
+"DANBOCCHI" is a personal simple soundproof room made of cardboard sold by VIBE Inc.
+For details, please refer to the official website.
+http://www.danbocchi.com/en
+
+This asset contains 3D model of "DANBOCCHI".
+Since the child object is installed around the door hinge, you can make animation of opening and closing the door.
+Also, it is easy to show inside, because it is an individual object for each panel.
+
+Please confirm "DANBOCCHI usage guidelines" when using 3D model data.
+http://www.danbocchi.com/en-guidelines
+
+
+[How to use]
+Please use the "Danbocchi" prefab in the "Danbocchi/Prefabs" folder.
+Scenes of usage examples are in the "Danbocchi/Demo/Scenes" folder.
+
